461d66d05e10466c54c51958475b50157b9797d1
root <root@patrick-rkdb-01.evoxt.com>
2025-08-03 20:00:46 +0000

    resume

--- src/data/resume/degrees.ts
+++ src/data/resume/degrees.ts
@@ -7,17 +7,11 @@ export interface Degree {
 
 const degrees: Degree[] = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
-  },
-  {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
-  },
+    school: 'Singapore Management University',
+    degree: 'B.S. Computer Science',
+    link: 'https://www.smu.edu.sg/',
+    year: 2014,
+  }
 ];
 
 export default degrees;
